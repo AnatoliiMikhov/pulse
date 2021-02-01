567e7a9d9243cbeeec3223cb8fcfe98f04a429b1
AnatoliiMikhov <anatoli.mihov@gmail.com>
2021-02-01 23:53:10 +0200

    carousel swiper adapted

--- src/js/script.js
+++ src/js/script.js
@@ -4,4 +4,11 @@ const swiper = new Swiper('.swiper-container', {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
 	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	autoplay: {
+		delay: 2500,
+	}
 });
