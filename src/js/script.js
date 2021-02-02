ad7cb4c5096e5c18a27bd4b93a9fc25a2e86013c
AnatoliiMikhov <anatoli.mihov@gmail.com>
2021-02-02 23:23:44 +0200

    catalog tabs and cart screen done and adapted

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,38 @@
-const swiper = new Swiper('.swiper-container', {
-	loop: true,
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
-	pagination: {
-		el: '.swiper-pagination',
-		clickable: true,
-	},
-	autoplay: {
-		delay: 2500,
+$(document).ready(function () {
+
+	const swiper = new Swiper('.swiper-container', {
+		loop: true,
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+		},
+		autoplay: {
+			delay: 2500,
+		}
+	});
+
+	// tabs script
+	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+		$(this)
+			.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+			.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+	});
+
+	function toggleTabs(item) {
+		$(item).each(function (i) {
+			$(this).on('click', function (e) {
+				e.preventDefault();
+				$('.catalog-item__first').eq(i).toggleClass('catalog-item__first_active');
+				$('.catalog-item__second').eq(i).toggleClass('catalog-item__second_active');
+			})
+		});
 	}
+
+	toggleTabs('.catalog-item__link');
+	toggleTabs('.catalog-item__back');
+
 });
